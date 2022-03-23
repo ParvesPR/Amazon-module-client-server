899f43a9bc36a3193f162a8ce8502623738ee44c
ParvesPR <parves2585@gmail.com>
2022-03-23 14:19:37 +0800

    order summury added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const { cart } = props
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        console.log(product)
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = (total + shipping + tax).toFixed(2);
     return (
-        <div>
-            <h3>Order Summury</h3>
+        <div className='cart'>
+            <h2 className='order-title'>Order Summury</h2>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal}</h4>
         </div>
     );
 };

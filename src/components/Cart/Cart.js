225ad836bd0498a2a006453d7494098b5c5d4fd3
ParvesPR <parves2585@gmail.com>
2022-03-27 23:46:21 +0800

    data show from local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,8 +6,8 @@ const Cart = (props) => {
     let shipping = 0;
     let quantity = 0;
     for (const product of cart) {
-        quantity = product.quantity + quantity;
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
     const tax = parseFloat((total * 0.1).toFixed(2));

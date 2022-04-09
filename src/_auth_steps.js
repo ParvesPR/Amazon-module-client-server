1059538895904f93c4f3bb07a84cfd817a38359a
ParvesPR <parves2585@gmail.com>
2022-04-10 06:38:56 +0800

    initail firebase project

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -2,7 +2,7 @@
 * create a new project in console.google.com.
 * npm install firebase.
 * create firebase init.js and import auth to export auth.
-*
+* firebase>setting> Authentication enable Email and password
  */
 
 /* 

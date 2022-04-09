909172d0e44f8aa294447edf02519b80a852efec
ParvesPR <parves2585@gmail.com>
2022-04-10 06:34:12 +0800

    initial firebase project

---
+++ src/_auth_steps.js
@@ -0,0 +1,13 @@
+/* 
+* create a new project in console.google.com.
+* npm install firebase.
+* create firebase init.js and import auth to export auth.
+*
+ */
+
+/* 
+* Hosting Steps
+* firebase login
+* firebase init
+* firebase deploy
+ */

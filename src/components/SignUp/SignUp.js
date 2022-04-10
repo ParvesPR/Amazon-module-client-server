ed78cfd1aed707cc9d1ea175339d6b99f45380cc
ParvesPR <parves2585@gmail.com>
2022-04-10 14:43:01 +0800

    sign up page added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form>
+                    <div className='input-group'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" required/>
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" required/>
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" name="confirm-password" required/>
+                    </div>
+                    <input className='form-btn' type="submit" value="Sign Up" />
+                </form>
+                <p>Already have an account? <Link className='create-ac-link' to='/login'>Login</Link></p>
+                <div className='or-div'>
+                    <div className='div-1'><hr /></div> 
+                    <span> or </span>
+                    <div className='div-2'><hr /></div>
+                </div>
+                <div className='continue-google'>
+                    Continue with Google
+                </div>
+            </div>
         </div>
     );
 };

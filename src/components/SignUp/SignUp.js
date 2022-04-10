f57ef6db6632c40bb93829a9294f46396fb3e1f7
ParvesPR <parves2585@gmail.com>
2022-04-10 14:59:07 +0800

    password match condition added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmail = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePassword = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirm = event => {
+        setConfirm(event.target.value);
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if(password !== confirm){
+            setError("Password didn't match")
+            return;
+        }
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Sign Up</h2>
-                <form>
+                <form onSubmit={handleCreateUser}>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" required/>
+                        <input onBlur={handleEmail} type="email" name="email" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" required/>
+                        <input onBlur={handlePassword} type="password" name="password" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="confirm-password">Confirm Password</label>
-                        <input type="password" name="confirm-password" required/>
+                        <input onBlur={handleConfirm} type="password" name="confirm-password" required />
                     </div>
+                    <p className='show-error-msg'>{error}</p>
                     <input className='form-btn' type="submit" value="Sign Up" />
                 </form>
                 <p>Already have an account? <Link className='create-ac-link' to='/login'>Login</Link></p>
                 <div className='or-div'>
-                    <div className='div-1'><hr /></div> 
+                    <div className='div-1'><hr /></div>
                     <span> or </span>
                     <div className='div-2'><hr /></div>
                 </div>

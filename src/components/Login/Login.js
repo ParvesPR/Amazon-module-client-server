dc54f67601cb57b8d06f6883fd8f4164e17b6606
ParvesPR <parves2585@gmail.com>
2022-04-11 13:40:10 +0800

    login page added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmail = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePassword = event => {
+        setPassword(event.target.value);
+    };
+    if(user){
+        navigate('/shop');
+    }
+    const handleLogin = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
+
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Login</h2>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" required/>
+                        <input onBlur={handleEmail} type="email" name="email" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" required/>
+                        <input onBlur={handlePassword} type="password" name="password" required />
                     </div>
+                    <p>{error?.message}</p>
+                    {loading && <p>Loading...</p>}
                     <input className='form-btn' type="submit" value="Login" />
                 </form>
                 <p>New to Amazon? <Link className='create-ac-link' to='/signup'>Create an account</Link></p>
                 <div className='or-div'>
-                    <div className='div-1'><hr /></div> 
+                    <div className='div-1'><hr /></div>
                     <span> or </span>
                     <div className='div-2'><hr /></div>
                 </div>

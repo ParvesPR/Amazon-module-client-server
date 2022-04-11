3926bfbe74fb8d9b0462c670fb0b066bea143860
ParvesPR <parves2585@gmail.com>
2022-04-11 15:07:57 +0800

    shipping cart added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
@@ -11,9 +14,6 @@ const Shipment = () => {
     const handleName = event => {
         setName(event.target.value);
     }
-    const handleEmail = event => {
-        setEmail(event.target.value);
-    }
 
     const handleAddress = event => {
         setAddress(event.target.value);
@@ -21,19 +21,25 @@ const Shipment = () => {
     const handlePhone = event => {
         setPhone(event.target.value);
     }
+    const handleCreateUser = event => {
+        event.preventDefault();
+        const shipping = { name, email, address, phone };
+        console.log(shipping);
+
+    }
 
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Shipping Info</h2>
-                <form>
+                <form onSubmit={handleCreateUser}>
                     <div className='input-group'>
                         <label htmlFor="name">Name</label>
                         <input onBlur={handleName} type="text" name="name" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input onBlur={handleEmail} type="email" name="email" required />
+                        <input value={user?.email} readOnly type="email" name="email" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="address">Address</label>
